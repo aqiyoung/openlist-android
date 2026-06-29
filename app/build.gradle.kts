60ff1896ef12d0706f009bf84b41f035dc70e99f
aqiyoung <aqiyoung@users.noreply.github.com>
2026-06-29 21:02:06 +0800

    fix: 补充 Brush + clickable import + 版本 0.3.75

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.threel.openlist"
         minSdk = 26  // Android 8.0
         targetSdk = 34
-        versionCode = 74
-        versionName = "0.3.74"
+        versionCode = 75
+        versionName = "0.3.75"
     }
 
     buildTypes {
